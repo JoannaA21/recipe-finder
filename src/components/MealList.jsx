6b08b35bc56cb0388ee0bca8d1f42d7f067e2123
Joanna Apellido <joanne14.ja@gmail.com>
2025-01-24 17:57:52 -0700

    removed weird 0 in the UI

--- src/components/MealList.jsx
+++ src/components/MealList.jsx
@@ -9,19 +9,19 @@ const MealList = ({ meals }) => {
     <div className="container mx-auto px-4 pt-10">
       {/* Meals by selected category */}
       <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-4">
-        {categoryList &&
-          categoryList.map((meal) => (
-            <MealCard key={meal.idMeal} meal={meal} />
-          ))}
+        {categoryList.map((meal) => (
+          <MealCard key={meal.idMeal} meal={meal} />
+        ))}
       </div>
 
       {/* Meals with instructions and YouTube links */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {searchedList.length &&
-          searchedList.map((meal) => (
+      {searchedList.length > 0 && (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {searchedList.map((meal) => (
             <MealCard key={meal.idMeal} meal={meal} />
           ))}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
